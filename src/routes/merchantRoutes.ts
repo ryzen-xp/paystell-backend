d8fd0a5bb53cc55f6e1692f9c8c6c6b36b547394
josephchimebuka <chimebujoseph@gmail.com>
2025-03-29 15:29:50 +0100

    feat: Add business endpoints

---
+++ src/routes/merchantRoutes.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import { authenticateMerchant, asyncHandler } from '../middlewares/merchantAuth';
+import { MerchantController } from '../controllers/merchant.controller';
+import { FileUploadService } from '../../src/services/fileUpload.service';
+import { handleFileUpload } from '../../src/middlewares/fileUploadMiddleware';
+
+const router = Router();
+const merchantController = new MerchantController();
+const fileUploadService = new FileUploadService();
+
+router.get(
+  '/profile',
+  authenticateMerchant,
+  asyncHandler(merchantController.getProfile.bind(merchantController))
+);
+
+router.post(
+  '/profile',
+  asyncHandler(merchantController.registerMerchant.bind(merchantController))
+);
+
+router.put(
+  '/profile',
+  authenticateMerchant,
+  asyncHandler(merchantController.updateProfile.bind(merchantController))
+);
+
+router.post(
+  '/logo',
+  authenticateMerchant,
+  fileUploadService.upload.single('logo'),
+  handleFileUpload,
+  asyncHandler(merchantController.uploadLogo.bind(merchantController))
+);
+
+router.delete(
+  '/logo',
+  authenticateMerchant,
+  asyncHandler(merchantController.deleteLogo.bind(merchantController))
+);
+
+export default router;
